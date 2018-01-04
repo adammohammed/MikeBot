c951e9433bf6a12380a4ba0e1607de5b6a3c1622
Adam Mohammed <adammoham@gmail.com>
2018-01-04 14:14:52 -0500

    Mikebot without update sql logging

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/adammohammed/groupmebot"
+)
+
+/*
+ Test hook functions
+ Each hook should match a certain string, and if it matches
+ it should return a string of text
+ Hooks will be traversed until match occurs
+*/
+func hello(msg groupmebot.InboundMessage) string {
+	resp := fmt.Sprintf("Hi, %v.", msg.Name)
+	return resp
+}
+
+func hello2(msg groupmebot.InboundMessage) string {
+	resp := fmt.Sprintf("Hello, %v.", msg.Name)
+	return resp
+}
+
+func main() {
+
+	bot, err := groupmebot.NewBotFromJson("mybot_cfg.json")
+	if err != nil {
+		log.Fatal("Could not create bot structure")
+	}
+
+	// Make a list of functions
+	bot.AddHook("Hi!$", hello)
+	bot.AddHook("Hello!$", hello2)
+
+	// Create Server to listen for incoming POST from GroupMe
+	log.Printf("Listening on %v...\n", bot.Server)
+	http.HandleFunc("/", bot.Handler())
+	log.Fatal(http.ListenAndServe(bot.Server, nil))
+}
